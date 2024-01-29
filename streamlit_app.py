fa9993d9f5b6f87e61ac828fa398febbfa6fedea
D Vamsee <141542075+DVamsee@users.noreply.github.com>
2024-01-29 17:03:37 +0000

    added files

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,17 @@ LOGGER = get_logger(__name__)
 
 import streamlit as st
 import streamlit_authenticator as stauth
+import numpy as np
+import pandas as pd
 import os
+from streamlit_folium import st_folium
+import folium
+
 
 from dependencies import is_admin, is_staff, is_user
 
 import yaml
 from yaml.loader import SafeLoader
-
-cwd = os.getcwd()
 
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
@@ -39,33 +42,58 @@ def reset_password_form():
         except Exception as e:
             st.error(e)
 
+st.title('Home Page')
+
 
 authenticator.login()
 
+
 if st.session_state["authentication_status"]:
     with st.sidebar:
+        username = st.session_state["username"]
+        if is_admin(config, username) or is_staff(config, username):
+            with st.expander('Register new User'):
+                try:
+                    role = st.selectbox(f'Select the type of user to register', ['admin', 'staff', 'user'], index=2)
+                    email, username, name = authenticator.register_user(fields={"Form name":''}, preauthorization=False)
+                    if username:
+                        if role == 'admin':
+                            config['roles']['admin'].append(username)
+                        if role == 'staff':
+                            config['roles']['staff'].append(username)
+                        if role == 'user':
+                            config['roles']['user'].append(username)
+                        with open('config.yaml', 'w') as file:
+                            yaml.dump(config, file, default_flow_style=False)
+                        st.success(f'User {name} registered as {role}')
+                except Exception as e:
+                    st.error(e)
         reset_password = st.button('Reset Password',on_click=reset_password_form)
         authenticator.logout()
-    
-    st.title('Home Page')
-    username = st.session_state["username"]
-    if is_admin(config, username) or is_staff(config, username):
-        try:
-            st.subheader('Register User')
-            role = st.selectbox(f'Select the type of user to register', ['admin', 'staff', 'user'], index=2)
-            email, username, name = authenticator.register_user(fields={"Form name":''}, preauthorization=False)
-            if username:
-                if role == 'admin':
-                    config['roles']['admin'].append(username)
-                if role == 'staff':
-                    config['roles']['staff'].append(username)
-                if role == 'user':
-                    config['roles']['user'].append(username)
-                with open('config.yaml', 'w') as file:
-                    yaml.dump(config, file, default_flow_style=False)
-                st.success(f'User {name} registered as {role}')
-        except Exception as e:
-            st.error(e)
+        
+    tab1, tab2 = st.tabs(["Maps", "folium"])
+
+    with tab1:
+        # map representation
+        map_data = pd.DataFrame(
+            np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+            columns=['lat', 'lon']
+        )
+        st.map(map_data)
+
+    with tab2:
+        m = folium.Map(
+            location = [13.517391973459102, 79.49937735696051],
+            zoom_start = 16,
+        )
+        folium.Marker(
+            [13.517391973459102, 79.49937735696051],
+            popup = "Home ",
+            tooltip = "Home",
+        ).add_to(m)
+
+        st_folium(m, width=1200)
+
             
 
 

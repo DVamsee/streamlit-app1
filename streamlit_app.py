ee380c4dc31f47f364db56ec64ee60a2b79bf4ed
D Vamsee <141542075+DVamsee@users.noreply.github.com>
2024-01-30 03:51:30 +0000

    asdsdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,37 +82,36 @@ if st.session_state["authentication_status"]:
         st.map(map_data)
 
     with tab2:
-        DATE_COLUMN = 'date/time'
-        DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+        # DATE_COLUMN = 'date/time'
+        # DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+        #         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+        
+        # def load_data(nrows):
+        #     data = pd.read_csv(DATA_URL, nrows=nrows)
+        #     lowercase = lambda x: str(x).lower()
+        #     data.rename(lowercase, axis='columns', inplace=True)
+        #     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        #     return data
 
-        @st.cache_data
-        def load_data(nrows):
-            data = pd.read_csv(DATA_URL, nrows=nrows)
-            lowercase = lambda x: str(x).lower()
-            data.rename(lowercase, axis='columns', inplace=True)
-            data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-            return data
 
+        # data = load_data(10000)
 
-        data = load_data(10000)
+        # if st.checkbox('Show raw data'):
+        #     st.subheader('Raw data')
+        #     st.write(data)
 
-        if st.checkbox('Show raw data'):
-            st.subheader('Raw data')
-            st.write(data)
+        # st.subheader('Number of pickups by hour')
+        # hist_values = np.histogram(
+        #     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+        # st.bar_chart(hist_values)
 
-        st.subheader('Number of pickups by hour')
-        hist_values = np.histogram(
-            data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-        st.bar_chart(hist_values)
-
-        """
-        ## Map of all pickups
-        """
-        hour_to_filter = st.slider('hour', 0, 23, 17) # min: 0h, max: 23h, default: 17h
-        filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-        st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-        st.map(filtered_data)
+        # """
+        # ## Map of all pickups
+        # """
+        # hour_to_filter = st.slider('hour', 0, 23, 17) # min: 0h, max: 23h, default: 17h
+        # filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+        # st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+        # st.map(filtered_data)
 
             
 

e66bcaa9955d64df4492532ff53fe8dbedc083fc
D Vamsee <141542075+DVamsee@users.noreply.github.com>
2024-01-30 03:31:14 +0000

    added uber data

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if st.session_state["authentication_status"]:
         reset_password = st.button('Reset Password',on_click=reset_password_form)
         authenticator.logout()
         
-    tab1, tab2 = st.tabs(["Maps", "folium"])
+    tab1, tab2 = st.tabs(["Maps", "Uber"])
 
     with tab1:
         # map representation
@@ -82,17 +82,41 @@ if st.session_state["authentication_status"]:
         st.map(map_data)
 
     with tab2:
-        m = folium.Map(
-            location = [13.517391973459102, 79.49937735696051],
-            zoom_start = 16,
-        )
-        folium.Marker(
-            [13.517391973459102, 79.49937735696051],
-            popup = "Home ",
-            tooltip = "Home",
-        ).add_to(m)
+        DATE_COLUMN = 'date/time'
+        DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+                'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-        st_folium(m, width=1200)
+        @st.cache_data
+        def load_data(nrows):
+            data = pd.read_csv(DATA_URL, nrows=nrows)
+            lowercase = lambda x: str(x).lower()
+            data.rename(lowercase, axis='columns', inplace=True)
+            data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+            return data
+
+
+        data_load_state = st.text('Loading data...')
+
+        data = load_data(10000)
+
+        data_load_state.text("Done! (using st.cache_data)")
+
+        if st.checkbox('Show raw data'):
+            st.subheader('Raw data')
+            st.write(data)
+
+        st.subheader('Number of pickups by hour')
+        hist_values = np.histogram(
+            data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+        st.bar_chart(hist_values)
+
+        """
+        ## Map of all pickups
+        """
+        hour_to_filter = st.slider('hour', 0, 23, 17) # min: 0h, max: 23h, default: 17h
+        filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+        st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+        st.map(filtered_data)
 
             
 
